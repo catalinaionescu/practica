8f610c24459ec3d82c1a0f7de8bff26380b9de7c
catalinaionescu <catalinamaria.ionescu@gmail.com>
2024-07-09 14:31:04 +0300

    Update main.py

--- cazino/main.py
+++ cazino/main.py
@@ -1,3 +1,21 @@
+
+
+''' Scrieți un program care să ajute la detectarea trișorilor. În cazul în care se detectează că o carte apare
+de prea multe ori, programul va afișa cartea și va opri jocul.
+Date de intrare
+Se vor citi de la tastatură (fluxul stdin) următoarele date:
+• Pe prima linie se află numărul n, reprezentând numărul maxim de mâini ce vor fi jucate;
+• Pe următoarele n linii se află cartea jucată, în formatul:
+<număr_carte> <semn_carte>
+Date de ieşire
+În cazul în care nimeni nu încearcă să trișeze se va afișa textul JOC OK. În cazul în care cineva a
+încercat sa trișeze, se va afișa cartea problemă în același format ca la intrare: numărul cărții urmat de
+semn, separate prin spațiu. '''
+
+'''de facut: inlocuit input cu trimisul datelor din fisiere
+fis sa fie argum la momentul rularii adica main.py joc1.txt 
+de uit la mod sys.args sys.argv
+'''
 n = int(input())
 m = {}
 ok = 0
@@ -9,9 +27,11 @@ for i in range(n):
     if m[key] > 2 and ok == 0:
         sus = key
         ok = 1
+        break
 
 if ok == 1:
-    print(sus)
+    #print(f'\n{sus}')
+    print('\n', sus, sep='')
 else:
     print("JOC OK")
 
