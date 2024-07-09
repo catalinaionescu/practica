5b0e36ae37b66acb547be2c96d5153dd78ab9ebe
catalinaionescu <catalinamaria.ionescu@gmail.com>
2024-07-09 15:48:07 +0300

    probleme

--- cinema/main.py
+++ cinema/main.py
@@ -21,6 +21,7 @@ Programul va afișa pe ecran (stream-ul standard de ieșire) un singur șir de c
 numele filmului ales, în formatul dat în datele de intrare.
 """
 
+
 # 1 linie ora la care ajung la cinema
 # n nr oferte disp
 # next linii <ora> <pret> <nume film>
@@ -31,7 +32,7 @@ numele filmului ales, în formatul dat în datele de intrare.
 
 def conv_min_to_int(timp):
     hours, minut = map(int, timp.split(":"))
-    tot_time = hours*60 + minut
+    tot_time = hours * 60 + minut
     return tot_time
 
 
@@ -40,16 +41,16 @@ n = int(input())
 lista = []
 for i in range(n):
     lista.append(input().split())
-mini_ora = 1440
-mini_pret = 100
+ora_minim = 1440
+pret_minim = 100
 for i in range(len(lista)):
     dif_tp = conv_min_to_int(lista[i][0]) - conv_min_to_int(me_time)
     if dif_tp > 0:
-        if dif_tp < mini_ora:
-            mini_ora = dif_tp
+        if dif_tp < ora_minim:
+            ora_minim = dif_tp
             nume = lista[i][2]
             pret = lista[i][1]
-        elif dif_tp == mini_ora:
+        elif dif_tp == ora_minim:
             if lista[i][1] < pret:
                 pret = lista[i][1]
                 nume = lista[i][2]
---
+++ nr palindromic max/main.py
@@ -0,0 +1,25 @@
+"""Un studiu de marketing cerut de o galerie de artă mai excentrică a arătat că, în mod surprinzător,
+clienţii analizează o perioadă mai mare de timp exponatele care au preţuri ce pot fi exprimate ca un
+palindrom şi de obicei le preferă, chiar şi în cazul în care un alt obiect similar are un preţ ceva mai
+scăzut. Pe de altă parte, din anumite motive (doar de ei ştiute) artiştii, care creează aceste produse,
+vor ca numerele care definesc preţul unui obiect de artă sa fie atent elaborate şi să conţină doar
+anumite cifre considerate de ei ca fiind foarte importante. Sub aceste constrângeri, proprietarii
+galeriei de artă vor să maximizeze câştigurile şi să afişeze cele mai mari preţuri posibile în
+condiţiile menţionate.
+Astfel, se cere să se realizeze un program care interpretează o secvenţă de cifre nenule oferite la
+intrare, în vederea obţinerii unor reprezentări zecimale (numere în bază 10). Plecând de la cifrele
+oferite sunt posibile mai multe reprezentări zecimale. Dintre numerele zecimale astfel obţinute se va
+alege cel mai mare număr care are proprietatea de palindrom (vedeţi explicaţiile de mai jos).
+Date de intrare
+La intrarea programului, pe prima linie se prezintă un număr natural n, iar pe a doua linie n cifre,
+separate prin câte un spaţiu. Cu aceste cifre se pot construi numere în baza 10.
+Fiecare dintre cele două linii de intrare se încheie cu caracterul newline (\n), obţinut prin apăsarea
+tastei Enter.
+Date de ieşire
+Să se determine numărul cel mai mare de tip palindrom (în situaţia în care se pot forma mai
+multe palindromuri) ce se poate forma cu toate cifrele conţinute pe a doua linie. Programul va afişa
+la consolă (pe stream-ul stdout) numărul astfel determinat urmat de caracterul newline (\n) (tasta
+Enter).
+Dacă nu se poate forma niciun palindrom, la ieşire se va afişa cifra 0 (zero); dacă se poate forma un
+singur palindrom, acesta, evident, se va afişa la ieşire ca atare."""
+
