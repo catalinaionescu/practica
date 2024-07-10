4c6b63feafc1b56edcd71276b8f63f8113938155
catalinaionescu <catalinamaria.ionescu@gmail.com>
2024-07-10 15:48:29 +0300

    probleme

---
+++ bomboane/main.py
@@ -0,0 +1,38 @@
+"""
+Andrei cumpără bomboane într-o anumită zi dacă el are destui bani de la tatăl său şi acest lucru duce la
+creşterea fericirii lui cu un anumit număr de puncte egal cu aroma bomboanei. Obligatoriu, dacă are destui bani, el va
+cumpăra bomboana. El păstrează toţi banii rămaşi după ce a cumpărat bomboanele pentru a-şi putea cumpăra mai multe
+următoarea zi. Dacă el nu are destui bani să îşi cumpere bomboane într-o anumită zi, asta nu înseamnă că este necesar
+ca punctele lui de fericire să scadă (evident că nici mai fericit nu îl vor face). Punctele de fericire vor scădea doar dacă
+el nu a cumpărat, în oricare din zilele trecute, cel puţin o bomboană cu o aromă mai bună decât cea din ziua curentă, pe
+care nu şi-o permite.
+Cerinţă
+Dându-se un număr n pozitiv, reprezentând numărul de zile în care băiatul primeşte bani de la tatăl său, apoi n numere
+pozitive, reprezentând suma de bani pe care o primeşte băiatul într-o anumită zi, apoi n perechi de numere pozitive, care
+reprezintă costul şi aroma bomboanelor din magazin într-o anumită zi, să se calculeze numărul de puncte de fericire al
+lui Andrei. Numărul de puncte de fericire va creşte sau va scădea cu numărul pozitiv prin care este reprezentată aroma,
+conform spuselor din enunţ. Presupunem că la început Andrei avea 0 lei şi 0 puncte de fericire.
+Date de intrare
+Pe prima linie se află numărul întreg pozitiv n, reprezentând numărul de zile în care băiatul primeşte bani de la tatăl său,
+urmat de caracterul newline. Pe următoarea linie se află n valori întregi pozitive, separate prin whitespace, reprezentând
+suma de bani pe care o primeşte băiatul într-o anumită zi, urmate de caracterul newline. Pe următoarele n linii se află
+perechi de numere întregi de forma "cost_bomboana aroma_bomboana", care reprezintă costul şi aroma
+bomboanelor din magazin în fiecare zi.
+Date de ieşire
+Se va afişa, pe o singură linie, un singur număr întreg, reprezentând numărul de puncte de fericire pe care le are Andrei
+la finalul celor n zile. Linia se va termina obligatoriu cu un caracter newline ("\n").
+"""
+
+# crestere fericire  = pct aroma bombo
+# mereu cumpara daca are bani si ce i ramane ramane pt next day
+# nu are bani intr o zi nu ii scade neaparat fericirea
+# ii scad punctele doar daca in orice zi de dinainte de azi nu si a luat cel put o bomboana mai buna decat cea de azi pe care nu si o permite
+# n nr de zile in care primeste bani
+# n sume de bani pe care le primeste zilnic pe o linie
+# n perechi cost aroma bombo pe n linii
+
+n = int(input())
+bani = list(map(int, input().split()))
+bombo = []
+for i in range(n):
+    bombo.append(list(map(int, input().split())))
--- nr palindromic max/main.py
+++ nr palindromic max/main.py
@@ -3,16 +3,16 @@ clienţii analizează o perioadă mai mare de timp exponatele care au preţuri c
 palindrom şi de obicei le preferă, chiar şi în cazul în care un alt obiect similar are un preţ ceva mai
 scăzut. Pe de altă parte, din anumite motive (doar de ei ştiute) artiştii, care creează aceste produse,
 vor ca numerele care definesc preţul unui obiect de artă sa fie atent elaborate şi să conţină doar
-anumite cifre considerate de ei ca fiind foarte importante. Sub aceste constrângeri, proprietarii
+anumite cifree considerate de ei ca fiind foarte importante. Sub aceste constrângeri, proprietarii
 galeriei de artă vor să maximizeze câştigurile şi să afişeze cele mai mari preţuri posibile în
 condiţiile menţionate.
-Astfel, se cere să se realizeze un program care interpretează o secvenţă de cifre nenule oferite la
+Astfel, se cere să se realizeze un program care interpretează o secvenţă de cifree nenule oferite la
 intrare, în vederea obţinerii unor reprezentări zecimale (numere în bază 10). Plecând de la cifrele
 oferite sunt posibile mai multe reprezentări zecimale. Dintre numerele zecimale astfel obţinute se va
 alege cel mai mare număr care are proprietatea de palindrom (vedeţi explicaţiile de mai jos).
 Date de intrare
-La intrarea programului, pe prima linie se prezintă un număr natural n, iar pe a doua linie n cifre,
-separate prin câte un spaţiu. Cu aceste cifre se pot construi numere în baza 10.
+La intrarea programului, pe prima linie se prezintă un număr natural n, iar pe a doua linie n cifree,
+separate prin câte un spaţiu. Cu aceste cifree se pot construi numere în baza 10.
 Fiecare dintre cele două linii de intrare se încheie cu caracterul newline (\n), obţinut prin apăsarea
 tastei Enter.
 Date de ieşire
@@ -21,5 +21,47 @@ multe palindromuri) ce se poate forma cu toate cifrele conţinute pe a doua lini
 la consolă (pe stream-ul stdout) numărul astfel determinat urmat de caracterul newline (\n) (tasta
 Enter).
 Dacă nu se poate forma niciun palindrom, la ieşire se va afişa cifra 0 (zero); dacă se poate forma un
-singur palindrom, acesta, evident, se va afişa la ieşire ca atare."""
+singur palindrom, acesta, evident, se va afişa la ieşire ca atare.
+Restricţii şi precizări
+1. Numărul n este un număr natural, în gama 2 < n < 20.
+2. Cifrele citite pe cea de-a doua linie sunt în gama [1; 9]."""
 
+
+# palindrom max cu toate cifrele print 0 daca nu gasesc :(
+
+def sortare(cifre):  # functie de sortare a aparitiilor cifrelor in ordine descrescatoare
+    for i in range(len(cifre) - 1):
+        for j in range(i+1, len(cifre)):
+            if cifre[i][1] < cifre[j][1]:
+                cifre[i], cifre[j] = cifre[j], cifre[i]
+            elif cifre[i][1] == cifre[j][1]:
+                if cifre[i][0] < cifre[j][0]:
+                    cifre[i], cifre[j] = cifre[j], cifre[i]
+    return cifre
+
+
+n = int(input())
+cifre = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0]]
+lista = list(map(int, input().split()))
+for i in lista:
+    cifre[i-1][1] += 1
+cifre = sortare(cifre)
+counter1 = 0  # un palindrom poate avea maxim o cifra care poate sa apara doar odata, daca sunt mai multe cifre atunci nu are rost sa mai continui
+criteriu = 1
+palindrom = ""
+cifra_jum = ""
+for i in range(len(cifre)):
+    if cifre[i][1] % 2 == 1:  # daca gasesc o cifra care apare de un nr impar de ori cresc counter ca sa vad cate cifre s-ar duce la mijloc
+        counter1 += 1
+        if counter1 > 1:
+            criteriu = 0
+            break
+        elif criteriu == 1:  # daca criteriu e 1 atunci pot sa retin cifra de mijloc
+            cifra_jum = str(cifre[i][0])
+    for j in range(int(cifre[i][1]/2)):  # construiesc palindromul doar pe jumate la print ii adaug si inversul
+        palindrom += str(cifre[i][0])
+
+if criteriu == 1:
+    print(palindrom + cifra_jum + palindrom[::-1])
+else:
+    print(0)
